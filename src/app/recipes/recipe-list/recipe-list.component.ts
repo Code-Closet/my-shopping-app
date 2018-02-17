d95b03396dd33a6b862f990a3ab62966ee26b5aa
sreekanth.pullat <sreekanth.pullat@visualiq.com>
2018-02-17 15:38:31 +0530

    Basic components check in

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: RecipeModel[] = [
+    new RecipeModel('New Recipe', 'Description',
+    'http://www.seriouseats.com/images/2017/04/20170405-one-pot-chicken-recipes-roundup-04.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
